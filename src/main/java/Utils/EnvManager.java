eedff3cc4851f072d454e9279a453d35f6a78355
KareemOwais <kareemmohamed70.km@gmail.com>
2025-05-19 19:37:18 +0300

    Secure Token Using ,env file and GitHub Secrets Try2

--- src/main/java/Utils/EnvManager.java
+++ src/main/java/Utils/EnvManager.java
@@ -2,15 +2,23 @@ package Utils;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class EnvManager {
-    private static final Dotenv dotenv = Dotenv.load();
-
-        public static String getEnvVar(String key) {
-            String value = System.getenv(key);    // For GitHub Actions
-            if (value == null) {
-                value = dotenv.get(key);          // Locally
-            }
-            return value;
+    private static Dotenv dotenv;
+    public static String getEnvVar(String key) {
+        try {
+            dotenv = Dotenv.load();
+        } catch (Exception e) {
+            System.out.println(".env File Not Found , Running in GitHub Actions");
+            dotenv = null;
         }
+        String value = System.getenv(key); // Get Environment variable from GitHub Secrets
+
+        if (value == null && dotenv != null) {
+            value = dotenv.get(key);
+        }
+
+        return value;
     }
+}
+
 
 
